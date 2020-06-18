96d43ddc35a4d7c3f4a450fd8bb841dc3456a3a5
rana-abhi777 <abhisekrana777@gmail.com>
2020-06-19 00:06:55 +0530

    Added a new comonent to app - named person

--- react-apps/react-complete-guide/src/App.js
+++ react-apps/react-complete-guide/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person.js';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello React App!</h1>
+      <Person />
     </div>
   );
 }
---
+++ react-apps/react-complete-guide/src/Person/Person.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import ReactDom from 'react-dom';
+
+const person = () => {
+    return (
+        <p>I am a Person!</p>
+    );
+};
+
+export default person;
