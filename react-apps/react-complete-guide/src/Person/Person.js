177ef4869a546846dadacb283ff79c73ad7d9a7e
rana-abhi777 <abhisekrana777@gmail.com>
2020-06-24 14:32:41 +0530

    undestanding props.children to fetch component values

--- react-apps/react-complete-guide/src/Person/Person.js
+++ react-apps/react-complete-guide/src/Person/Person.js
@@ -3,7 +3,11 @@ import React from 'react';
 
 const person = (props) => {
     return (
-    <p>I am a {props.name} and I am {props.age} years old.</p>
+        <div>
+            <p>I am a {props.name} and I am {props.age} years old.</p>
+            <p>{props.children}</p>
+        </div>
+    
     );
 };
 
